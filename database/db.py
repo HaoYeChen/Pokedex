1291285fbfa4683a0913e890ef49b36e167d0ea2
Hao <54834780+HaoYeChen@users.noreply.github.com>
2023-06-14 14:54:29 +0200

    deleted useless codes and stuff

--- database/db.py
+++
@@ -1,6 +0,0 @@
-import pyodbc
-
-def get_connection():
-    return pyodbc.connect("Driver=SQL Server; Server=HAO\SQLEXPRESS; Database=VoresDB; Trusted_Connection=yes; UID=sa; PWD=123456789")
-
-connection = get_connection()  # Establish a connection to the SQL Server database using the provided connection string
