c976ffe96eaa0178c924e0d6c3fd05ed41448a40
Hao <54834780+HaoYeChen@users.noreply.github.com>
2023-06-14 14:56:22 +0200

    added async

--- app.py
+++ app.py
@@ -1,29 +1,22 @@
 from flask import Flask, render_template, abort, request, redirect, flash, url_for
+import asyncio
 from services.pokemon_service import get_pokemon_list
 from services.pokemon_service import get_pokemon_details
-from services.trainer_service import create_trainer, read_trainers, update_trainer, delete_trainer
 
 app = Flask(__name__)
 
 '''
-index page
+pokedex page
 '''
 @app.route("/")
 def index():
-    return render_template("index.html")
-
-'''
-pokedex page
-'''
-@app.route("/pokedex")
-def pokemon():
-    pokemon_list = get_pokemon_list()
+    pokemon_list = asyncio.run(get_pokemon_list())
     return render_template("pokedex.html", pokemon_list=pokemon_list)
 
 '''
 pokemon details page
 '''
-@app.route("/pokedex/<pokemon_id>")
+@app.route("/<pokemon_id>")
 def pokemon_details(pokemon_id):
     # Convert pokemon_id to an integer
     pokemon_id = int(pokemon_id)
@@ -70,38 +63,6 @@ def pokemon_search():
 @app.errorhandler(404)
 def not_found_error(error):
     return render_template('404.html'), 404
-
-'''
-Trainers page
-'''
-# Get all trainers & pokemons
-@app.route('/trainers')
-def trainers():
-    trainers = read_trainers()
-    return render_template('trainers.html', trainers=trainers)
-
-# Creates trainer & pokemon
-@app.route('/trainers', methods=['POST'])
-def add_trainer():
-    name = request.form.get('name')
-    pokemon = request.form.get('pokemon')
-    create_trainer(name, pokemon)  # Pass the parameters to the create_trainer function
-    return redirect('/trainers')
-
-# Update trainer
-@app.route('/trainers/<int:trainer_id>', methods=['GET', 'POST'])
-def edit_trainer(trainer_id):
-    if request.method == 'POST':
-        name = request.form.get('name')
-        pokemon = request.form.get('pokemon')
-        update_trainer(trainer_id, name, pokemon)
-        return redirect('/trainers')
-
-# Delete trainer
-@app.route('/trainers/delete/<int:trainer_id>', methods=['POST'])
-def delete(trainer_id):
-    delete_trainer(trainer_id)
-    return redirect('/trainers')
 
 
 if __name__ == "__main__":
--- services/pokemon_service.py
+++ services/pokemon_service.py
@@ -1,30 +1,45 @@
+import aiohttp
+import asyncio
 import requests
-
-session = requests.Session()
-
 
 pokemon_cache = {}
 
-# Display all Pokemon
-def get_pokemon_list():
+
+async def get_pokemon_data(url):
+    if url in pokemon_cache:
+        return pokemon_cache[url]
+
+    try:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url) as response:
+                response.raise_for_status()
+                pokemon_data = await response.json()
+                pokemon_cache[url] = pokemon_data
+                return pokemon_data
+
+    except (aiohttp.ClientError, ValueError) as e:
+        print(f"An error occurred: {e}")
+        return {}
+
+
+async def get_pokemon_list():
     pokemon_list = []
-    url = "https://pokeapi.co/api/v2/pokemon?limit=151"
+    url = "https://pokeapi.co/api/v2/pokemon?limit=1000"
 
     try:
         response = requests.get(url)
-        # Raise an exception if the response status code is not successful
         response.raise_for_status()
         results = response.json()["results"]
 
-        for result in results:
-            pokemon_data = get_pokemon_data(result["url"])
+        pokemon_urls = [result["url"] for result in results]
+        pokemon_data = await asyncio.gather(*[get_pokemon_data(url) for url in pokemon_urls])
 
-            # Extract relevant data for each Pokemon and add it to the pokemon_list
+        for data in pokemon_data:
             pokemon = {
-                "name": pokemon_data["name"].capitalize(),
-                "image": pokemon_data['sprites']['other']['official-artwork']['front_default'],
-                "type": pokemon_data["types"][0]["type"]["name"].capitalize(),
-                "id": pokemon_data["id"]
+                "name": data["name"].capitalize(),
+                "image": data['sprites']['other']['official-artwork']['front_default'],
+                "type": data["types"][0]["type"]["name"].capitalize(),
+                "id": data["id"]
             }
             pokemon_list.append(pokemon)
 
@@ -34,77 +49,55 @@ def get_pokemon_list():
     return pokemon_list
 
 
-def get_pokemon_data(url):
-    if url in pokemon_cache:
-        return pokemon_cache[url]
-
-    try:
-        response = requests.get(url)
-        # Raise an exception if the response status code is not successful
-        response.raise_for_status()
-        pokemon_data = response.json()
-        pokemon_cache[url] = pokemon_data  # Cache the fetched Pokemon data
-        return pokemon_data
-
-    except (requests.RequestException, ValueError) as e:
-        print(f"An error occurred: {e}")
-        return {}
-
-
-pokemon_list = get_pokemon_list()
-
-# Get details of a specific Pokemon
 def get_pokemon_details(pokemon_id):
     url = f"https://pokeapi.co/api/v2/pokemon/{pokemon_id}"
 
     try:
-        response = session.get(url)
-        # Raise an exception if the response status code is not successful
-        response.raise_for_status()
-        data = response.json()
+        with requests.Session() as session:
+            response = session.get(url)
+            response.raise_for_status()
+            data = response.json()
 
-        # Extract detailed information about the Pokemon
-        pokemon = {
-            "name": data["name"].capitalize(),
-            "image": data['sprites']['other']['official-artwork']['front_default'],
-            # Convert height from decimeters to meters
-            "height": data["height"] / 10,
-            # Convert weight from hectograms to kilograms
-            "weight": data["weight"] / 10,
-            "abilities": [ability["ability"]["name"].capitalize() for ability in data["abilities"]],
-            "types": [type_data["type"]["name"].capitalize() for type_data in data["types"]],
-            # Get the weaknesses of the Pokemon
-            "weaknesses": get_weaknesses(data),
-            # Get the base stats of the Pokemon
-            "stats": get_stats(data["stats"])
-        }
-        return pokemon
+            pokemon = {
+                "name": data["name"].capitalize(),
+                "image": data['sprites']['other']['official-artwork']['front_default'],
+                "height": data["height"] / 10,
+                "weight": data["weight"] / 10,
+                "abilities": [ability["ability"]["name"].capitalize() for ability in data["abilities"]],
+                "types": [type_data["type"]["name"].capitalize() for type_data in data["types"]],
+                "weaknesses": get_weaknesses(data),
+                "stats": get_stats(data["stats"])
+            }
+            return pokemon
 
     except (requests.RequestException, ValueError) as e:
         print(f"An error occurred: {e}")
 
     return None
 
-# Get weaknesses of a Pokemon
+
 def get_weaknesses(data):
     weaknesses = set()
 
     for type_data in data["types"]:
         type_url = type_data["type"]["url"]
-        type_response = session.get(type_url)
-        type_data = type_response.json()
 
-        if "damage_relations" in type_data:
-            for damage_relation in type_data["damage_relations"]["double_damage_from"]:
-                # Add types that are weak against the Pokemon to the weaknesses set
-                weaknesses.add(damage_relation["name"].capitalize())
+        try:
+            with requests.Session() as session:
+                type_response = session.get(type_url)
+                type_response.raise_for_status()
+                type_data = type_response.json()
+
+                if "damage_relations" in type_data:
+                    for damage_relation in type_data["damage_relations"]["double_damage_from"]:
+                        weaknesses.add(damage_relation["name"].capitalize())
+
+        except (requests.RequestException, ValueError) as e:
+            print(f"An error occurred: {e}")
 
     return list(weaknesses)
 
 
-# Get base stats of a Pokemon
 def get_stats(stats):
     stat_names = ["hp", "attack", "defense", "speed"]
-
-    # Extract the base stats for the specified stat names
     return {stat["stat"]["name"]: stat["base_stat"] for stat in stats if stat["stat"]["name"] in stat_names}
